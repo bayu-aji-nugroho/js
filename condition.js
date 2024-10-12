ce7a44a84e27d6f16b518c3563501dbe3edaddbb
bayu-aji-nugroho <bayuastudies@gmail.com>
2024-10-12 09:12:43 +0700

    add file

---
+++ condition.js
@@ -0,0 +1,18 @@
+(10 < 2) ? console.log("BENAR") : console.log("SALAH");
+
+
+function hello (name,age){
+    return console.log(name,age(10));
+    
+}
+
+
+hello("Bayu Aji Nugroho", (umur) => console.log("umur anda " + umur))
+
+
+
+
+
+
+
+
